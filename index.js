77862158087295b72b1ba6aad19e81213c699a73
Mary Marchini <mmarchini@netflix.com>
2020-08-27 20:47:15 -0700

    feat: create issue with ngrok link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const core = require('@actions/core')
-// const octokit = require('@actions/github');
+const github = require('@actions/github');
 const fastify = require('fastify')
 const fastq = require('fastq')
 const path = require('path')
@@ -11,7 +11,10 @@ const { writeFile, unlink } = require('fs').promises
 const exec = promisify(require('child_process').exec)
 
 const NPMRC_PATH = path.join(process.cwd(), '.npmrc')
-const NPM_TOKEN = process.env.NPM_TOKEN || core.getInput('token')
+const NPM_TOKEN = process.env.NPM_TOKEN || core.getInput('npm_token')
+const context = github.context;
+const githubToken = core.getInput('github_token');
+const octokit = githubToken ? github.getOctokit(githubToken) : null;
 
 async function npmPublish (otp, log) {
   // TODO(mmarchini): take existing file into account
@@ -67,6 +70,15 @@ try {
 
     const ngrokUrl = await ngrok.connect(3000)
     app.log.info({ ngrokUrl }, 'ngrok connected')
+    if (octokit) {
+      app.log.info('creating issue')
+      await octokit.issues.create({
+        ...context.repo,
+        title: 'Provide OTP for release', // TODO(mmarchini): show version number
+        body: ngrokUrl // TODO(mmarchini): add more info to the issue
+      })
+      app.log.info('issue created')
+    }
     // TODO(mmarchini): create issue
   })
 } catch (error) {

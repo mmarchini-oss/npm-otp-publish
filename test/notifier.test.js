5e962d38538d27416ab7d624696fb3e105783959
Mary Marchini <mmarchini@netflix.com>
2020-08-29 22:21:46 -0700

    fix(notifier): more tests and loggers

--- test/notifier.test.js
+++ test/notifier.test.js
@@ -2,9 +2,12 @@
 
 const tape = require('tape')
 const test = require('tape-promise').default(tape)
-const { Notifier } = require('../lib/notifier')
 const { Octokit } = require('@octokit/rest')
 const nock = require('nock')
+
+const { Notifier } = require('../lib/notifier')
+const { getOptions } = require('../lib/cli')
+const { getConfig } = require('../lib/config')
 
 const mockLogger = {
   info: () => {},
@@ -26,6 +29,29 @@ test('should create and close issue', async t => {
 
   const octokit = new Octokit('***')
   const notifier = new Notifier({ notifier: 'github-issue', octokit, repo: { owner, repo } }, mockLogger)
+  await notifier.notify()
+  scope.done()
+
+  await notifier.end()
+  scope2.done()
+  t.end()
+})
+
+test('should create and close issue on Actions', async t => {
+  const repo = 'xyz'
+  const owner = 'acme'
+  const scope = nock('https://api.github.com')
+    .post(`/repos/${owner}/${repo}/issues`)
+    .reply(201, { number: 123 })
+  const scope2 = nock('https://api.github.com')
+    .patch(`/repos/${owner}/${repo}/issues/123`, { state: 'closed' })
+    .reply(200)
+
+  // const config = { notifier: 'github-issue', octokit, repo: { owner, repo } }
+  const env = { GITHUB_ACTIONS: true, GITHUB_REPOSITORY: `${owner}/${repo}` }
+  const opts = await getOptions(['--githubToken=***', '--npmToken=***', '--notifier=github-issue'], env)
+  const config = getConfig(opts)
+  const notifier = new Notifier(config, mockLogger)
   await notifier.notify()
   scope.done()
 
